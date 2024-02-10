98bd28be1ca4e74a88d01b1f36e55b73fce70788
Y0o0Y1 <Y0o0Y12>
2024-02-10 19:49:35 +0200

    typo fixed

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import { I18nextProvider } from 'react-i18next'
 import { Provider } from 'react-redux'
 import { RouterProvider } from 'react-router-dom'
-import "./app.css"
+import "./App.css"
 import { RTL } from './components/rtl'
 import i18n from './i18n/i18n'
 import { store } from './redux/store'
